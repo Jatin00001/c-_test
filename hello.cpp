30a708c2537be8c1762121fc6b6aebe171a2fdc5
Jatin <datajatin90@gmail.com>
2023-03-11 23:55:08 +0530

    .

--- hello.cpp
+++ hello.cpp
@@ -2,8 +2,7 @@
 using namespace std;
 
 int main(){
-    cout<<"hello world !";
-
-
+    int n;cin>>n;
+    cout<<n<<endl;
     return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,31 +1,8 @@
-# include <iostream>
-# include <climits>
+#include<iostream>
 using namespace std;
+int main()
+{
 
-int setBits(int n)
-{ 
-	int ans  = 0;
-	while (n!=0)
-	{
-		int bit = n & 1;
-		if(bit == 1)
-		{
-			ans++;
-		}
-		n = n >>1 ;
-	}
-	return ans;
-}
-int main() 
-{ 
-	int n ;
-	int m;
-	cout<<"Enter First value" <<endl;
-	cin>>m;
-	cout<<"Enter Second value" <<endl;
-	cin>>n ;
-	// cout<<"Sets bits in "<< m <<" -  "<<setBits(m)<<endl;
-	// cout<<"Sets bits in "<< n <<" -  "<<setBits(n)<<endl;
-	cout<<"Total setbits in first and second is :- "<< setBits(m) + setBits(n)<<endl;
-	
+    cout<<"Hello World !";
+    return 0;
 }
