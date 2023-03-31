dc7d88ea6af40ac15c945d8ed934135faeaec13e
Jatin <datajatin90@gmail.com>
2023-03-31 21:35:45 +0530

    /

---
+++ copy_string.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    string s = {'q','c','s','a','s','s','s'};
+    string b;
+    b.append(s);
+    cout<<b<<"   <---- b is"<<endl;
+
+
+    return 0;
+}
