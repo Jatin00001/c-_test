a7335844a81d3f6443d1e515b4a8d92607537618
Jatin <datajatin90@gmail.com>
2023-07-06 22:47:49 +0530

    add

--- arrays/tempCodeRunnerFile.cpp
+++ arrays/tempCodeRunnerFile.cpp
@@ -1,1 +1,8 @@
-i
+array<int,9>a{1,2,3,4,5,1,2,3,4};
+    xor use for we know xor  0 ^ 0 = 0 && 0 ^ 1 = 1
+    int n = 0;
+    for(int i = 0;i<a.size();i++)
+    {
+        n = n ^ a[i];
+    }
+    cout<<n<<endl;
