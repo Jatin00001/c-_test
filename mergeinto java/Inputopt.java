3ae677dd7505b3bcec3739881653c6e9071113ea
keshav chaudhary <141941257+Keshav-Chaudhary@users.noreply.github.com>
2023-08-25 22:58:44 +0530

    input/opt

---
+++ mergeinto java/Inputopt.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Inputopt {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a  = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("a is  " + a + "  \nb is   " + b);
+    }
+    
+}
